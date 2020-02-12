4852fbfe7a7479441cc8a7184133f3a227bc7e3e
Zolotukhin Fedor <bat312@yandex.ru>
2020-02-12 19:10:33 +0300

    Make iterationMethod module

--- dz2.py
+++ dz2.py
@@ -1,50 +1,10 @@
 from utils import readMatrixFromFile
-
-def divideIntoTwoMatrices(matrix):
-    C, D = list(), list()
-
-    for i, row in enumerate(matrix):
-        C.append(list())
-        D.append(list())
-        
-        for j, elem in enumerate(row):
-            
-            if i == j:
-                C[i].append(0)
-                D[i].append(elem)
-            else:
-                C[i].append(elem)
-                D[i].append(0)
-
-    return [C, D]
-
-def getInverseMatrix(matrix): 
-    raise NotImplementedError
-
-def multMatrixByMatrix(m1, m2):
-    raise NotImplementedError
-
-def substractMatrixFromMatrix(m1, m2):
-    raise NotImplementedError
-
-def computeQ(matrix):
-    raise NotImplementedError
-
-def getFuncAndQ(A, y):
-    [C, D] = divideIntoTwoMatrices(A)
-    inverseC = getInverseMatrix(C);
-    B = multMatrixByMatrix(inverseC, D)
-    newY = multMatrixByMatrix(inverseC, y)
-    q = computeQ(B)
-
-    def F(x):
-        return substractMatrixFromMatrix(newY, multMatrixByMatrix(B, x))
-    
-    return [F, q]
+from iterationMethod import findRoot
 
 A1 = readMatrixFromFile('input22_A1.txt')
 y1 = readMatrixFromFile('input22_y1.txt')
+print('First root:', findRoot(A1, y1))
 
-[f, q] = getFuncAndQ(A1, y1)
-
-# TODO: Implement iterations
+A2 = readMatrixFromFile('input22_A2.txt')
+y2 = readMatrixFromFile('input22_y2.txt')
+print('Second root:', findRoot(A2, y2))
---
+++ iterationMethod.py
@@ -0,0 +1,67 @@
+from math import sqrt
+
+def divideIntoTwoMatrices(matrix):
+    C, D = [], []
+
+    for i, row in enumerate(matrix):
+        C.append([])
+        D.append([])
+        
+        for j, elem in enumerate(row):
+            
+            if i == j:
+                C[i].append(0)
+                D[i].append(elem)
+            else:
+                C[i].append(elem)
+                D[i].append(0)
+
+    return [C, D]
+
+def getInverseMatrix(matrix): 
+    raise NotImplementedError
+
+def multMatrixByMatrix(m1, m2):
+    raise NotImplementedError
+
+def substractMatrixFromMatrix(m1, m2):
+    raise NotImplementedError
+
+def computeQ(matrix):
+    sums = []
+    
+    for i, row in enumerate(matrix):
+        list.append(0)
+
+        for j, elem in enumerate(row):
+            if j != i:
+                list[i] += elem
+
+    return max(sums)
+
+def getFuncAndQ(A, y):
+    [C, D] = divideIntoTwoMatrices(A)
+    inverseC = getInverseMatrix(C);
+    B = multMatrixByMatrix(inverseC, D)
+    newY = multMatrixByMatrix(inverseC, y)
+    q = computeQ(B)
+
+    def F(x):
+        return substractMatrixFromMatrix(newY, multMatrixByMatrix(B, x))
+    
+    return [F, q]
+
+def findRoot(A, y):
+    [f, q] = getFuncAndQ(A, y)
+    x = [0 for elem in y]
+    eps = input("Epsilon: ")
+    treshhold = eps * (q-1) / q
+
+    while True:
+        oldX = x
+        x = f(x)
+
+        if sqrt( sum( list( map(lambda x: x*x,  substractMatrixFromMatrix(x, oldX)) ) ) ) < treshhold:
+            break;
+    
+    return x
